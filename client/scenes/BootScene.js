7d32c4b75561f0400439d07c66e327428dbb0e1e
IdkWhatImdoingahhhhh <634159@trinity.vic.edu.au>
2025-09-10 14:38:45 +1000

    Delete client directory

--- client/scenes/BootScene.js
+++
@@ -1,8 +0,0 @@
-export default class BootScene extends Phaser.Scene {
-    constructor(){ super({ key:'BootScene' }); }
-    preload(){
-        this.load.spritesheet('player','assets/sprites/player.png',{ frameWidth:32, frameHeight:32 });
-        this.load.image('background','assets/backgrounds/level1.png');
-    }
-    create(){ this.scene.start('MenuScene'); }
-}
--- client/scenes/MenuScene.js
+++
@@ -1,8 +0,0 @@
-export default class MenuScene extends Phaser.Scene {
-    constructor(){ super({ key:'MenuScene' }); }
-    create(){
-        this.add.text(300,250,'Tag2 Clone',{fontSize:'32px',fill:'#fff'});
-        this.add.text(250,300,'Press SPACE to Start',{fontSize:'24px',fill:'#fff'});
-        this.input.keyboard.on('keydown-SPACE',()=>this.scene.start('GameScene'));
-    }
-}
