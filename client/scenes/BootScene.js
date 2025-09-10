91e8bd8d524a46e53f543a84375fff801bf98234
IdkWhatImdoingahhhhh <634159@trinity.vic.edu.au>
2025-09-10 15:03:24 +1000

    Delete client directory

--- client/scenes/BootScene.js
+++
@@ -1,8 +0,0 @@
-export default class BootScene extends Phaser.Scene {
-    constructor(){ super({ key: 'BootScene' }); }
-    preload(){
-        this.load.spritesheet('player','assets/sprites/player.png',{ frameWidth:32, frameHeight:32 });
-        this.load.image('map','assets/backgrounds/map1.png');
-    }
-    create(){ this.scene.start('MenuScene'); }
-}
