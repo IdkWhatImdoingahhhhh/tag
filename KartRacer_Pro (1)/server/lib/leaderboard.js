9397ce2b8f93f08927b621446529af43f0f6d23d
IdkWhatImdoingahhhhh <634159@trinity.vic.edu.au>
2025-09-10 17:28:29 +1000

    Delete KartRacer_Pro (1) directory

--- KartRacer_Pro (1)/server/lib/leaderboard.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-class Leaderboard {
-  constructor(path){ this.path = path; if (!fs.existsSync(path)) fs.writeFileSync(path, JSON.stringify({scores:[]}, null, 2)); }
-  get(){ return JSON.parse(fs.readFileSync(this.path)); }
-  add(name, score){ const d = this.get(); d.scores.push({ name, score, date: new Date().toISOString() }); d.scores.sort((a,b)=>b.score-a.score); d.scores = d.scores.slice(0,100); fs.writeFileSync(this.path, JSON.stringify(d,null,2)); }
-}
-module.exports = Leaderboard;
