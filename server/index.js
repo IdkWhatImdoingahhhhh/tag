b2f82b37b94c159d4b368c1bfec0c2a819edf5c6
IdkWhatImdoingahhhhh <634159@trinity.vic.edu.au>
2025-09-11 15:01:19 +1000

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+const clientPath = path.resolve(__dirname, "../client");
+app.use(express.static(clientPath));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(clientPath, "index.html"));
+});
+
+let players = {};
+
+io.on('connection', socket => {
+  players[socket.id] = { x: 0, z: 50, yaw: 0, speed: 0, lap: 0 };
+  socket.on('input', data => {
+    if (players[socket.id]) Object.assign(players[socket.id], data);
+  });
+  socket.on('disconnect', () => { delete players[socket.id]; });
+});
+
+setInterval(() => { io.emit('state', players); }, 50);
+server.listen(PORT, () => console.log('Server running on port', PORT));
