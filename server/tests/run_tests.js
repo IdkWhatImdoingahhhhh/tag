78c7cbd046e72ebd4083d47c704b11621d231c0c
IdkWhatImdoingahhhhh <634159@trinity.vic.edu.au>
2025-09-11 14:33:17 +1000

    Delete server directory

--- server/tests/run_tests.js
+++
@@ -1,4 +0,0 @@
-import { tickPhysics } from '../lib/physics.js';
-let p = { x:0,y:0,z:0,yaw:0,speed:0 };
-tickPhysics(p, { throttle:1, steer:0, drift:false }, 1/20, { checkpoints:[] });
-console.log('Physics tick OK', p.x, p.z);
